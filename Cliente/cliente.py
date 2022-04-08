29609d49b5120556e3b1b9c8df51f7a6ad1c1f83
Alvaro Plata <ad.plata@uniandes.edu.co>
2022-04-08 13:47:47 -0500

    Primera implementacion exitosaaa

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -9,19 +9,45 @@ cant_clientes = 1
 
 cant_threads = 0
 
-def thread_cliente(clientSock):
-    clientSock.connect((UDP_IP_ADDRESS, UDP_PORT_NO))
 
-    with open('./archivos_recibidos/'+ "archivo", "wb") as f:
+def thread_cliente(clientSock, address):
+
+    clientSock.sendto(b"Transmitir", address)
+
+    # f = open('./archivos_recibidos/' + "archivo.txt", "wb")
+    # while True:
+    #     try:
+    #         bytes_read, address = clientSock.recvfrom(BUFFER_SIZE)
+    #     except socket.timeout as e:
+    #         print(e)
+    #         break
+    #     print(bytes_read)
+    #     print(bytes_read.decode())
+    #     if(bytes_read):
+    #         print("Sigo en el loop")
+    #         f.write(bytes_read)
+    #     else:
+    #         print("No más looops")
+    #         break
+    # print("Salí del loop")
+    with open('./archivos_recibidos/'+ "archivo.txt", "wb") as f:
             while True:
                 bytes_read, address = clientSock.recvfrom(BUFFER_SIZE)
-                clientSock.settimeout(60)
-                f.write(bytes_read)
+                print(bytes_read)
+                print(bytes_read.decode())
+                if(bytes_read == b"Archivo enviado completamente"):
+                    break
+                else:
+                    f.write(bytes_read)
+            print("Salí del loop")
+    clientSock.close()
+    print("Cerré el socket")
 
 while cant_threads < cant_clientes:
 
     clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = (UDP_IP_ADDRESS, UDP_PORT_NO)
 
-    threading.Thread(target=thread_cliente, args=(Client, ClientUDP, server_address, )).start()
+    threading.Thread(target=thread_cliente, args=(clientSock, server_address, )).start()
 
     cant_threads += 1
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -7,21 +7,11 @@ UDP_PORT_NO = 6789
 BUFFER_SIZE = 16384
 
 
-def thread_client(soc_client):
+def thread_client(soc_client, address):
     global UDP_PORT_NO
-    try:
-        print("Antes del exception")
-        print(soc_client)
-        print(soc_client.recvfrom(1024))
-        msg, address = soc_client.recvfrom(1024)
-        print("Address:")
-        print(address)
-    except Exception as e:
-        print("Hey, un error")
-        print(e)
     UDP_PORT_NO += 1
     print("address:",address)
-    with open("./archivos/100MB.txt", "rb") as f:
+    with open("./archivos/test.txt", "rb") as f:
         while True:
             # leer los bytes del archivo
             data_read = f.read(BUFFER_SIZE)
@@ -32,18 +22,20 @@ def thread_client(soc_client):
             # Enviar el paquete por el socket UDP
             soc_client.sendto(data_read, address)
 
+    soc_client.sendto(b"Archivo enviado completamente", address)
+    print("Terminé de enviar todo el archivo")
+
     # Cerrar la conexion del socket
     soc_client.close()
 
 
 
 while True:
-    print("Escuchando clientes")
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-    print("hey1")
+    print("Esperando conexión de un cliente")
     msg, address = serverSock.recvfrom(1024)
-    print("hey2")
+    print("Un cliente se conectó")
     threading.Thread(target=thread_client, args=(
-        serverSock,)).start()
+        serverSock, address)).start()
     print(UDP_PORT_NO)
